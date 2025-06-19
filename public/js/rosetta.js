302bcb0e511c3954562b3a3b510456daf9b39f47
Polo García <laga.1193@gmail.com>
2025-06-18 20:04:35 -0600

    Create rosetta.js

---
+++ public/js/rosetta.js
@@ -0,0 +1,73 @@
+{
+  "interface": {
+    "progress": {
+      "answered_questions": "0",
+      "total_questions": "0",
+      "slash": "/"
+    },
+    "navigation": {
+      "continue_button": "Continue",
+      "previous_button": "previous", 
+      "submit_button": "Submit",
+      "send_answers_button": "Enviar respuestas"
+    },
+    "instructions": {
+      "radio_selection": "Selecciona una de las siguientes opciones",
+      "checkbox_selection": "Selecciona una o mas de las siguientes opciones",
+      "keyboard_hint": " (puedes presionar la tecla debajo de cada opcion, para seleccionarla)",
+      "navigation_hint": "puedes presionar enter para continuar o las flechas ← → para navegar"
+    },
+    "completion_message": {
+      "thanks": "Thanks for answering this form"
+    }
+  },
+  "validation_messages": {
+    "required_field": "Este campo es requerido",
+    "valid_email": "e-mail valido",
+    "invalid_email": "por favor ingresa un mail valido",
+    "valid_number": "numero valido", 
+    "invalid_number": "por favor ingresa solo numeros en este campo",
+    "valid_date": "fecha valida",
+    "invalid_date": "fecha invalida, por favor ingresa una fecha valida"
+  },
+  "example_questions": {
+    "email_question": "Ingresa tu email",
+    "name_question": "¿Quien eres? Ingresa tu nombre",
+    "gender_question": "Whats your gender?",
+    "gender_select": "Selecciona uno de los siguientes",
+    "preferences_question": "¿Que de estos te gustaria ganar?",
+    "generic_placeholder": "Input here your question"
+  },
+  "example_options": {
+    "gender": {
+      "male": "Male",
+      "female": "Female", 
+      "other": "Other"
+    },
+    "preferences": {
+      "iphone": "iphone",
+      "ipad": "ipad", 
+      "money": "Dinero",
+      "anything": "Lo que sea es bueno"
+    }
+  },
+  "campus_survey_specific": {
+    "title": "Campus ITESM CQ",
+    "questions": {
+      "semester": "¿Que semestre te encuentras cursando?",
+      "career": "¿Que carrera estudias?",
+      "all_campus": "¿Haz cursado toda tu carrera en este campus?",
+      "cafeteria": "¿Haz comido en alguna de las cafeterías dentro del campus?",
+      "cafeteria_followup": "En caso de haber respondido si, a la anterior.",
+      "info_3": "Fill the following information 3",
+      "info_4": "Fill the following information 4"
+    },
+    "yes_no_options": {
+      "yes": "Si",
+      "no": "No"
+    }
+  },
+  "keyboard_shortcuts": {
+    "option_keys": ["A", "B", "C", "D", "E"]
+  }
+}
